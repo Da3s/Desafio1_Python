d4bbf0c3b1e0d62aa6f5f0dfdeef186815558f54
Daniel <danielmorernoinf@gmail.com>
2024-03-11 20:15:31 -0300

    se eliminan lineas de prueba

--- escape.py
+++ escape.py
@@ -5,19 +5,13 @@ import math
 # Declaracion de variables
 
 radio = float(input('Ingrese radio(km)):'))
-print(type(radio)) 
 
 constante = float(input('Ingrese la constante g(m/s2):'))
-print(type(constante)) 
 
 constante = constante * 1000
 
 
 resultado = (math.sqrt(2*radio*constante))
-
-
-
-print(type(resultado)) 
 
 print('La velocidad de escape es ', round(resultado,1))
 
